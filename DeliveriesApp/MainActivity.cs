d7bc3920b61dc431a09c5757ac9d6df2eb0275a0
miraj0072004@gmail.com <miraj0072004@gmail.com>
2020-07-06 14:19:24 +1000

    Add project files.

---
+++ DeliveriesApp/MainActivity.cs
@@ -0,0 +1,39 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+
+namespace DeliveriesApp
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        EditText namEditText;
+        Button helloButton;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            namEditText = FindViewById<EditText>(Resource.Id.nameEditText);
+            helloButton = FindViewById<Button>(Resource.Id.helloButton);
+
+            helloButton.Click += HelloButton_Click;
+        }
+
+        private void HelloButton_Click(object sender, System.EventArgs e)
+        {
+            Toast.MakeText(this,$"Hello {namEditText.Text}", ToastLength.Long).Show();
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
