dadd10f71179f16e02051c8daf01c73e2de322d6
miraj0072004@gmail.com <miraj0072004@gmail.com>
2020-07-11 12:38:02 +1000

    The Login Page

--- DeliveriesApp/MainActivity.cs
+++ DeliveriesApp/MainActivity.cs
@@ -9,8 +9,10 @@ namespace DeliveriesApp
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
-        EditText namEditText;
-        Button helloButton;
+        EditText emailEditText;
+        EditText passwordEditText;
+        Button signinButton;
+        Button registerButton;
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
@@ -18,16 +20,32 @@ namespace DeliveriesApp
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
 
-            namEditText = FindViewById<EditText>(Resource.Id.nameEditText);
-            helloButton = FindViewById<Button>(Resource.Id.helloButton);
+            emailEditText = FindViewById<EditText>(Resource.Id.emailEditText);
+            passwordEditText = FindViewById<EditText>(Resource.Id.passwordEditText);
+            signinButton = FindViewById<Button>(Resource.Id.signinButton);
+            registerButton = FindViewById<Button>(Resource.Id.registerButton);
 
-            helloButton.Click += HelloButton_Click;
+            signinButton.Click += SigninButton_Click;
+            registerButton.Click += RegisterButton_Click;
+
+
+
         }
 
-        private void HelloButton_Click(object sender, System.EventArgs e)
+        private void RegisterButton_Click(object sender, System.EventArgs e)
         {
-            Toast.MakeText(this,$"Hello {namEditText.Text}", ToastLength.Long).Show();
+            
         }
+
+        private void SigninButton_Click(object sender, System.EventArgs e)
+        {
+            
+        }
+
+        //private void HelloButton_Click(object sender, System.EventArgs e)
+        //{
+        //    Toast.MakeText(this,$"Hello {namEditText.Text}", ToastLength.Long).Show();
+        //}
 
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
