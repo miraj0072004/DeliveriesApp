79a99cd588d0ee64fd98c4ce98065162d25635a0
miraj0072004@gmail.com <miraj0072004@gmail.com>
2020-07-21 13:49:22 +1000

    Navigating Between Fragments

--- DeliveriesApp/TabsActivity.cs
+++ DeliveriesApp/TabsActivity.cs
@@ -7,20 +7,51 @@ using Android.App;
 using Android.Content;
 using Android.OS;
 using Android.Runtime;
+using Android.Support.Design.Widget;
+using Android.Support.V4.App;
 using Android.Views;
 using Android.Widget;
+using Fragment = Android.Support.V4.App.Fragment;
 
 namespace DeliveriesApp
 {
     [Activity(Label = "TabsActivity")]
-    public class TabsActivity : Activity
+    public class TabsActivity : FragmentActivity
     {
+        private TabLayout tabLayout;
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             SetContentView(Resource.Layout.Tabs);
 
+            tabLayout = FindViewById<TabLayout>(Resource.Id.mainTabLayout);
+            tabLayout.TabSelected += TabLayout_TabSelected;
             // Create your application here
+
+            FragmanetNavigate(new DeliveriesFragment());
+        }
+
+        private void TabLayout_TabSelected(object sender, TabLayout.TabSelectedEventArgs e)
+        {
+            switch (e.Tab.Position)
+            {
+                case 0:
+                    FragmanetNavigate(new DeliveriesFragment());
+                    break;
+                case 1:
+                    FragmanetNavigate(new DeliveredFragment());
+                    break;
+                case 2:
+                    FragmanetNavigate(new ProfileFragment());
+                    break;
+            }
+        }
+
+        private void FragmanetNavigate(Fragment fragment)
+        {
+            var transaction = SupportFragmentManager.BeginTransaction();
+            transaction.Replace(Resource.Id.contentFrame, fragment);
+            transaction.Commit();
         }
     }
 }
