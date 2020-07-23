f045092385c36616c256e20a956b73272da00f9e
miraj0072004@gmail.com <miraj0072004@gmail.com>
2020-07-23 17:39:00 +1000

    Inserting Deliveries

--- DeliveriesApp/Delivery.cs
+++ DeliveriesApp/Delivery.cs
@@ -15,6 +15,19 @@ namespace DeliveriesApp
     public class Delivery
     {
         public string Id { get; set; }
+        public string Name { get; set; }
+        public double OriginLatitude { get; set; }
+        public double OriginLongitude { get; set; }
+        public double DestinationLatitude { get; set; }
+        public double DestinationLongitude { get; set; }
+        /// <summary>
+        /// 0 = waiting delivery person
+        /// 1 = being delivered
+        /// 2 = delivered
+        /// </summary>
+        public int Status { get; set; }
+
+
 
         public static async Task<List<Delivery>> GetDeliveries()
         {
@@ -24,6 +37,11 @@ namespace DeliveriesApp
                 await AzureHelper.MobileService.GetTable<Delivery>().ToListAsync();
 
             return deliveries;
+        }
+
+        public static async Task<bool> InsertDelivery(Delivery delivery)
+        {
+            return await AzureHelper.Insert<Delivery>(delivery);
         }
     }
 }
--- DeliveriesApp/NewDeliveryActivity.cs
+++ DeliveriesApp/NewDeliveryActivity.cs
@@ -15,12 +15,36 @@ namespace DeliveriesApp
     [Activity(Label = "NewDeliveryActivity")]
     public class NewDeliveryActivity : Activity
     {
+        private Button saveButton;
+        private EditText packageNamEditText;
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
 
             // Create your application here
             SetContentView(Resource.Layout.NewDelivery);
+
+            saveButton = FindViewById<Button>(Resource.Id.saveButton);
+            packageNamEditText = FindViewById<EditText>(Resource.Id.packageNameEditText);
+
+            saveButton.Click += SaveButton_Click;
+        }
+
+        private async void SaveButton_Click(object sender, EventArgs e)
+        {
+            Delivery delivery = new Delivery{
+                Name = packageNamEditText.Text,
+                Status = 0
+            };
+            var deliveryStatus = await Delivery.InsertDelivery(delivery);
+            if (deliveryStatus)
+            {
+                Toast.MakeText(this, "Delivery Added", ToastLength.Long).Show();
+            }
+            else
+            {
+                Toast.MakeText(this, "Failed to add delivery", ToastLength.Long).Show();
+            }
         }
     }
 }
